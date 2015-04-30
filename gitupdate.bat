e283cb7a6de3347a7d249b25918694bb6568ef7b
ztyler <ztyler@atl.yearup.org>
2015-04-30 10:37:19 -0400

    test batch 3

--- gitupdate.bat
+++ gitupdate.bat
@@ -1,11 +1,19 @@
 @ECHO OFF
-ECHO 4DD1NG t3h F1L3S
+ECHO --------------------------------
+ECHO 4DD1NG t3h F1L3S!11!!!1!!!!!
+ECHO --------------------------------
+
 git add -A
 
-ECHO C0MM1771NG t3h FILES
+ECHO --------------------------------
+ECHO C0MM1771NG t3h FILES!!!!1!!1
+ECHO --------------------------------
+
 git commit
 
-ECHO S3ND1NG FILEZ TO T3H INT3RW3BZ
+ECHO --------------------------------
+ECHO S3ND1NG FILEZ TO T3H INT3RW3BZ!!
+ECHO --------------------------------
 git push
 
-ECHO totes done
+ECHO done!
