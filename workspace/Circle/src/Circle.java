0b750ec837dc59f2bf9a64c76a4092dd13738abc
ztyler <ztyler@atl.yearup.org>
2015-04-30 13:41:57 -0400

    message

--- workspace/Circle/src/Circle.java
+++ workspace/Circle/src/Circle.java
@@ -1,6 +1,25 @@
 
 public class Circle {
-	public static void main(String[] args) {
-		
+	
+	double m_radius;
+	String m_color;
+	
+	Circle(double radius) {
+		m_radius = radius;
+		m_color = "grey";
+	}
+	
+	Circle(double radius, String color) {
+		m_radius = radius;
+		m_color = color;
+	}
+	
+	
+	double calcArea() {
+		return Math.PI * m_radius * m_radius;
+	}
+	
+	void printColor() {
+		System.out.println(m_color);
 	}
 }
