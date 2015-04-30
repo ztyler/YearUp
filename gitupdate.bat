5c58e4faeb10ce0f78f1642d9e3c268e92b14518
ztyler <ztyler@atl.yearup.org>
2015-04-30 11:15:27 -0400

    message

--- gitupdate.bat
+++ gitupdate.bat
@@ -1,26 +1,26 @@
 @ECHO OFF
 CLS
-ECHO --------------------------------
-ECHO ADDING ALL NEW FILES
-ECHO --------------------------------
+ECHO --------------------------------------------
+ECHO ADDING ALL NEW FILES (git add -A)
+ECHO --------------------------------------------
 
 git add -A
 
 ECHO files added
 
-ECHO --------------------------------
-ECHO COMMITTING CHANGES
-ECHO --------------------------------
+ECHO --------------------------------------------
+ECHO COMMITTING CHANGES (git commit)
+ECHO --------------------------------------------
 
 set /p message="Enter commit message: "
 
 git commit -m message
 
-ECHO --------------------------------
-ECHO SENDING FILES TO GITHUB.COM
-ECHO --------------------------------
+ECHO --------------------------------------------
+ECHO SENDING FILES TO GITHUB.COM (git push)
+ECHO --------------------------------------------
 git push
 
-ECHO --------------------------------
+ECHO --------------------------------------------
 ECHO done!
-ECHO --------------------------------
+ECHO --------------------------------------------
