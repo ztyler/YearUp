db18bafe3dfd6f73af293b4044f4fcf477665354
unknown <ztyler116@PC193.student.YearUp.org>
2015-05-07 14:22:23 -0500

    message

---
+++ workspace/.metadata/.plugins/org.eclipse.core.resources/.history/f/604fe443edf400141949d8737f1408c1
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Account savingsAccount = new Account(123, "Savings", 456);
+		savingsAccount.UpdateAmountSecurely(500, "admin");
+
+	}
+
+}
--- workspace/Account/src/Account.java
+++ workspace/Account/src/Account.java
@@ -1,17 +1,19 @@
 import static java.lang.System.out;
 
 public class Account {
-	public int m_accountId = -1,
+	private int m_accountId = -1,
 			m_customerId = -1;
-	public String m_accountName;
-	public double m_amount = 0.0;
+	private String m_accountName;
+	private double m_amount = 0.0;
 	
+	//Constructor
 	public Account(int accountId, String accountName, int customerId) {
 		m_accountId = accountId;
 		m_accountName = accountName;
 		m_customerId = customerId;
 	}
 	
+	//Actions
 	private void UpdateAmount(double newAmount) {
 		m_amount = newAmount;
 	}
@@ -20,7 +22,7 @@ public class Account {
 		if (password != null) {
 			if (password.equals("admin")) {
 				UpdateAmount(newAmount);
-				out.printf("Updated amount in account id %s to %d",
+				out.printf("Updated amount in account id %s to $%.2f\n",
 						m_accountId,
 						m_amount);
 			}
@@ -28,5 +30,33 @@ public class Account {
 				out.println("Call the police! Unsecure access!");
 			}
 		}
+	}
+	
+	//Getters
+	public int GetAccountId() {
+		return m_accountId;
+	}
+	public int GetCustomerId() {
+		return m_customerId;
+	}
+	public String GetAccountName() {
+		return m_accountName;
+	}
+	public double GetAmount() {
+		return m_amount;
+	}
+	
+	//Setters
+	public void SetAccountId(int accountId) {
+		m_accountId = accountId;
+	}
+	public void SetCustomerId(int customerId) {
+		m_customerId = customerId;
+	}
+	public void SetAccountName(String accountName) {
+		m_accountName = accountName;
+	}
+	public void SetAmount(double amount) {
+		m_amount = amount;
 	}
 }
