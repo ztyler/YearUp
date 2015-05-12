ce3d999188495728acf58e8df7943dc8d20aeb7d
ztyler <ztyler@atl.yearup.org>
2015-05-12 09:23:28 -0400

    message

---
+++ workspace/EncapProj/src/Person.java
@@ -0,0 +1,50 @@
+
+public class Person {
+	String m_name, m_phoneNum, m_address;
+	int m_age;
+	
+	Person(String name, String phoneNum, String address, int age) {
+		m_name = name;
+		m_phoneNum = phoneNum;
+		m_address = address;
+		m_age = age;
+	}
+	
+	void growsOlder() {
+		m_age++;
+	}
+	
+	//Getters
+	String getName() {
+		return m_name;
+	}
+	
+	String getPhoneNum() {
+		return m_phoneNum;
+	}
+	
+	String getAddress() {
+		return m_address;
+	}
+	
+	int getAge() {
+		return m_age;
+	}
+	
+	//Setters
+	void setName(String new_name) {
+		m_name = new_name;
+	}
+	
+	void setPhoneNum(String new_phoneNum) {
+		m_phoneNum = new_phoneNum;
+	}
+	
+	void setAddress(String new_address) {
+		m_address = new_address;
+	}
+	
+	void setAge(int new_age) {
+		m_age = new_age;
+	}
+}
