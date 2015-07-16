aa66aa74167a54c4eb95ac5dc4a1def00f8af31c
unknown <ztyler116@PC193.student.YearUp.org>
2015-07-16 11:32:40 -0400

    message

---
+++ workspace/.metadata/.plugins/org.eclipse.core.resources/.history/29/00e27644cd2b0015168d8ddd3ab1e4bd
@@ -0,0 +1,9 @@
+
+public class GameLogic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- workspace/Life/src/LifeGUI.java
+++ workspace/Life/src/LifeGUI.java
@@ -1,5 +1,4 @@
 import java.awt.Color;
-import java.awt.EventQueue;
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
@@ -19,27 +18,15 @@ import java.util.List;
 
 public class LifeGUI {
 
-	private static int _x = 25;
-	private static int _y = 25;
+	private static int _x = 50;
+	private static int _y = 50;
 	private static boolean _keepPlaying = true;
 	
-	private JFrame frame;
-	private JButton[][] grid = new JButton[_y][_x];
-	private Color alive = new Color(30, 200, 30);
+	JFrame frame;
+	JButton[][] grid = new JButton[_y][_x];
 	
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					LifeGUI window = new LifeGUI();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
+	static Color alive = new Color(30, 200, 30);
+	
 	public LifeGUI() {
 		initialize();
 	}
@@ -49,12 +36,19 @@ public class LifeGUI {
 		frame.setBounds(100, 100, 700, 700);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		JPanel panel_1 = new JPanel();
-		frame.getContentPane().add(panel_1, BorderLayout.SOUTH);
-		panel_1.setLayout(new GridLayout(0, 2, 0, 0));
+		JPanel controlPanel = new JPanel();
+		frame.getContentPane().add(controlPanel, BorderLayout.SOUTH);
+		controlPanel.setLayout(new GridLayout(0, 3, 0, 0));
 		
 		JButton startBtn = new JButton("Start");
-		panel_1.add(startBtn);
+		controlPanel.add(startBtn);
+		startBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				
+				
+			}
+		});
 		
 		JButton btnStop = new JButton("Stop");
 		btnStop.addActionListener(new ActionListener() {
@@ -64,27 +58,21 @@ public class LifeGUI {
 				
 			}
 		});
-		panel_1.add(btnStop);
-		startBtn.addActionListener(new ActionListener() {
+		
+		JButton btnStep = new JButton("Step");
+		btnStep.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				
-				startGame();
+				GameLogic.newGeneration();
 				
 			}
 		});
+		controlPanel.add(btnStep);
+		controlPanel.add(btnStop);
 		
 		JPanel body = new JPanel();
 		frame.getContentPane().add(body, BorderLayout.CENTER);
 		body.setLayout(new GridLayout(_y, _x, 0, 0));
-		
-		JMenuBar menuBar = new JMenuBar();
-		frame.setJMenuBar(menuBar);
-		
-		JMenu mnPresets = new JMenu("Presets");
-		menuBar.add(mnPresets);
-		
-		JMenuItem mntmTest = new JMenuItem("Test");
-		mnPresets.add(mntmTest);
 		
 		for (int y = 0; y < _y; y++)
 			for (int x = 0; x < _x; x++) {
@@ -97,7 +85,7 @@ public class LifeGUI {
 		
 	}
 	
-	private void setGridBtn(JButton btn) {
+	public void setGridBtn(JButton btn) {
 		
 		btn.addActionListener(new ActionListener() {
 			
@@ -111,68 +99,6 @@ public class LifeGUI {
 		
 	}
 	
-	private void startGame() {
-		
-		for (int y = 0; y < grid.length; y++)
-			for (int x = 0; x < grid[y].length; x++)
-				grid[y][x].setEnabled(false);
-		
-		List<JButton> birth = new ArrayList<JButton>();
-		List<JButton> death = new ArrayList<JButton>();
-		
-		for (int y = 0; y < grid.length; y++) {
-			for (int x = 0; x < grid[y].length; x++) {
-				
-				int liveNeighbors = 0;
-				
-				for (int i = -1; i < 2; i++) {
-					for (int n = -1; n < 2; n++) {
-						if (n == 0 && i == 0) continue;
-						
-						JButton btnCheck = null;
-						
-						try {
-							btnCheck = grid[y - i][x - n];
-						}
-						catch (ArrayIndexOutOfBoundsException e) {
-							continue;
-						}
-						
-						if (btnCheck.getBackground().equals(alive)) {
-							liveNeighbors++;
-						}
-						
-					}
-					
-				}
-					
-				if ((liveNeighbors >= 2 && liveNeighbors <= 3)) {
-						
-					birth.add(grid[y][x]);
-					
-				}
-				else {
-					
-					death.add(grid[y][x]);
-					
-				}
-				
-			}
-			
-		}
-		
-		for (JButton btn : birth) {
-			
-			btn.setBackground(alive);
-			
-		}
-		
-		for (JButton btn : death) {
-			
-			btn.setBackground(null);
-			
-		}
-		
-	}
+	
 	
 }
