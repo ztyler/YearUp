10ee2cf3bbd16d0a0f0571d48dc50825e7df4d53
unknown <ztyler116@PC193.student.YearUp.org>
2015-07-16 16:57:18 -0400

    message

--- workspace/Life/src/LifeGUI.java
+++ workspace/Life/src/LifeGUI.java
@@ -5,13 +5,12 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JFrame;
 import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
 import javax.swing.JMenu;
-import javax.swing.JCheckBoxMenuItem;
 import javax.swing.JRadioButtonMenuItem;
 
 public class LifeGUI {
@@ -25,15 +24,17 @@ public class LifeGUI {
 	
 	JFrame frame;
 	JPanel gridPanel, controlPanel;
-	JButton btnStart, btnStep, btnStop;
-	private JMenuBar menuBar;
-	private JMenu mnPresets;
+	JButton btnStart, btnStep, btnPause, btnStop;
+	JMenuBar menuBar;
+	JMenu menuPresets;
+	ButtonGroup presetsBtnGroup;
+	JRadioButtonMenuItem radioDefault, radioCheckerboard;
 	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					LifeGUI window = new LifeGUI(35, 35);
+					LifeGUI window = new LifeGUI(75, 75);
 					_logic = new GameLogic();
 					_continuous = new Thread(_logic);
 					
@@ -58,65 +59,120 @@ public class LifeGUI {
 		frame.setBounds(100, 100, 700, 700);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		//create grid
-		gridPanel = new JPanel();
-		frame.getContentPane().add(gridPanel, BorderLayout.CENTER);
-		if (_x != 0) {
-			gridPanel.setLayout(new GridLayout(_y, _x, 0, 0));
-		}
-		else {
-			gridPanel.setLayout(new GridLayout(25, 25, 0, 0));
-		}
+		buildMenu();
 		
-		for (int y = 0; y < _grid.length; y++) {
-			for (int x = 0; x < _grid[y].length; x++) {
-				
-				_grid[y][x] = new JButton();
-				setGridBtn(_grid[y][x]);
-				gridPanel.add(_grid[y][x]);
-				
-			}
-		}
+		buildCntrlPanel();
 		
-		//create control panel
+		buildGrid();
+		
+	}
+	
+	private void buildMenu() {
+		
+		menuBar = new JMenuBar();
+		frame.setJMenuBar(menuBar);
+		
+		menuPresets = new JMenu("Presets");
+		menuBar.add(menuPresets);
+		
+		presetsBtnGroup = new ButtonGroup();
+		
+		radioDefault = new JRadioButtonMenuItem("Default");
+		menuPresets.add(radioDefault);
+		presetsBtnGroup.add(radioDefault);
+		
+		radioCheckerboard = new JRadioButtonMenuItem("Checkerboard");
+		menuPresets.add(radioCheckerboard);
+		presetsBtnGroup.add(radioCheckerboard);
+	}
+	
+	private void buildCntrlPanel() {
 		controlPanel = new JPanel();
 		frame.getContentPane().add(controlPanel, BorderLayout.SOUTH);
-		controlPanel.setLayout(new GridLayout(0, 3, 0, 0));
+		controlPanel.setLayout(new GridLayout(0, 4, 0, 0));
 		
-		//populate control panel with buttons
+		btnStart = new JButton("Start");
 		btnStep = new JButton("Step");
+		btnPause = new JButton("Pause");
+		btnStop = new JButton("Stop");
+		
 		btnStep.setEnabled(false);
+		btnPause.setEnabled(false);
+		btnStop.setEnabled(false);
+		
+		btnStart.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				enableGrid(false);
+				btnPause.setEnabled(true);
+				btnStop.setEnabled(true);
+				if (_continuous.isAlive()) {
+					_logic.keepPlaying = true;
+				}
+				else {
+					_continuous.start();
+				}
+				
+				
+			}
+		});
+		
 		btnStep.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				_logic.newGeneration();
 			}
 		});
 		
-		btnStart = new JButton("Start");
-		btnStart.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				enableGrid(false);
-				_continuous.start();
-			}
-		});
-		
-		btnStop = new JButton("Stop");
-		btnStop.addActionListener(new ActionListener() {
+		btnPause.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				_logic.keepPlaying = false;
 				btnStep.setEnabled(true);
 			}
 		});
 		
+		btnStop.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				try
+				{
+				_logic.keepPlaying = false;
+				
+				while(!_logic.keepPlaying)
+				{
+					_continuous.sleep(500);
+				}
+				}
+				catch(Exception ex)
+				{
+					
+				}
+			}
+		});
+		
 		controlPanel.add(btnStart);
 		controlPanel.add(btnStep);
+		controlPanel.add(btnPause);
 		controlPanel.add(btnStop);
+	}
+	
+	private void buildGrid() {
+		gridPanel = new JPanel();
+		frame.getContentPane().add(gridPanel, BorderLayout.CENTER);
 		
-		menuBar = new JMenuBar();
-		frame.setJMenuBar(menuBar);
 		
-		mnPresets = new JMenu("Presets");
-		menuBar.add(mnPresets);
+		if (_x != 0) {
+			setGridSize(_y, _x);;
+		}
+		else {
+			setGridSize(25, 25);;
+		}
+		
+		for (int y = 0; y < _grid.length; y++) {
+			for (int x = 0; x < _grid[y].length; x++) {
+				_grid[y][x] = new JButton();
+				setGridBtn(_grid[y][x]);
+				gridPanel.add(_grid[y][x]);
+			}
+		}
 	}
 	
 	private void setGridBtn(JButton btn) {
@@ -132,7 +188,7 @@ public class LifeGUI {
 		gridPanel.setLayout(new GridLayout(y, x, 0, 0));
 	}
 	
-	private static void enableGrid(boolean option) {
+	private void enableGrid(boolean option) {
 		for (int y = 0; y < _grid.length; y++)
 			for (int x = 0; x < _grid[y].length; x++)
 				_grid[y][x].setEnabled(option);
--- workspace/Life/src/Template.java
+++ workspace/Life/src/Template.java
@@ -1,8 +1,4 @@
-
 public class Template {
-
-	Template() {
-		
-	}
+	
 	
 }
